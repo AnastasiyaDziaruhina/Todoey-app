edfb95b9edf4c243bd7169128a7c82574b617ff6
stacysfish <st.vitalievna@yandex.ru>
2022-06-16 18:46:56 +0200

    Initial Commit

---
+++ Todoey-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todoey-app
+//
+//  Created by Stacy on 16.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
